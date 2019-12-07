dc3a835bb553f52036813b59931b731d0fb3464a
Rachael Homann <rachaelhomann@gmail.com>
2019-12-07 15:15:53 -0600

    added server, organized route and html files

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,18 @@
+  
+const express = require("express");
+const apiRoutes = require("../Routes/apiRoutes");
+const htmlRoutes = require("../Routes/htmlRoutes");
+
+//init and create a port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//parse and route
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// start server
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
